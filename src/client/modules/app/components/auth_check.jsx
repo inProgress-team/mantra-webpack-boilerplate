f170664b2a9f4c5f99ebaa6099d7787bdc57f141
Théo mathieu <theo.mathieu@in-progress.io>
2016-06-02 12:52:09 -0400

    Login done + navbar added if auth checked

--- src/client/modules/app/components/auth_check.jsx
+++ src/client/modules/app/components/auth_check.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
 
 import { authComposer } from 'client/configs/composers.js';
 
@@ -9,7 +12,6 @@ class AuthCheck extends Component {
     if(loggingIn) return null;
 
     if(!user) {
-      console.log('plouf');
       setTimeout(()=>{
         FlowRouter.go('/login')
       });
@@ -17,8 +19,19 @@ class AuthCheck extends Component {
     }
 
     return (
-      <div>
-        AuthCheck
+      <div className="with-navbar">
+        <Navbar fixedTop fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Boilerplate</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Accueil</NavItem>
+            <NavItem eventKey={2} href="/new-post">New post</NavItem>
+          </Nav>
+        </Navbar>
 
         {content()}
       </div>
